4012f76dfec41d21f307044be8b5b467fa590fb0
x <x@MacBook-Pro-3.local>
2022-02-09 16:31:22 -0700

    add wiki links and .gitignore

--- src/pages/main.js
+++ src/pages/main.js
@@ -44,53 +44,188 @@ import totoroPls from "../assets/images/gifs/totoro-pls.gif";
 import totoroRain from "../assets/images/gifs/totoro-rain.gif";
 import waterReflection from "../assets/images/gifs/water-reflection.gif";
 
+const Studio_Ghibli =  "https://en.wikipedia.org/wiki/Studio_Ghibli";
+
+const gifs = [
+    {
+        src: arietty,
+        url: "https://en.wikipedia.org/wiki/Arrietty"
+    },
+    {
+        src: butterfly,
+        url: Studio_Ghibli
+    },
+    {
+        src: cat,
+        url: Studio_Ghibli
+    },
+    {
+        src: dragon,
+        url: "https://en.wikipedia.org/wiki/Spirited_Away"
+    },
+
+    {
+        src: ephemeris,
+        url: Studio_Ghibli
+    },
+    {
+        src: farming,
+        url: Studio_Ghibli
+    },
+    {
+        src: forestspirit,
+        url: "https://en.wikipedia.org/wiki/Princess_Mononoke"
+    },
+    {
+        src: lake,
+        url: "https://en.wikipedia.org/wiki/Kiki's_Delivery_Service"
+    },
+    {
+        src: howl,
+        url: "https://en.wikipedia.org/wiki/Howl's_Moving_Castle_(film)"
+    },
+    {
+        src: howlasleep,
+        url: "https://en.wikipedia.org/wiki/Howl's_Moving_Castle_(film)"
+    },
+    {
+        src: howllook,
+        url: "https://en.wikipedia.org/wiki/Howl's_Moving_Castle_(film)"
+    },
+    {
+        src: kaguya,
+        url: "https://en.wikipedia.org/wiki/The_Tale_of_the_Princess_Kaguya_(film)"
+    },
+    {
+        src: kaguya02,
+        url: "https://en.wikipedia.org/wiki/The_Tale_of_the_Princess_Kaguya_(film)"
+    },
+    {
+        src: kaguya03,
+        url: "https://en.wikipedia.org/wiki/The_Tale_of_the_Princess_Kaguya_(film)"
+    },
+    {
+        src: kaguyafarm,
+        url: "https://en.wikipedia.org/wiki/The_Tale_of_the_Princess_Kaguya_(film)"
+    },
+    {
+        src: kiki01,
+        url: "https://en.wikipedia.org/wiki/Kiki's_Delivery_Service"
+    },
+    {
+        src: kiki02,
+        url: "https://en.wikipedia.org/wiki/Kiki's_Delivery_Service"
+    },
+    {
+        src: kiki03,
+        url: "https://en.wikipedia.org/wiki/Kiki's_Delivery_Service"
+    },
+    {
+        src: mononoke01,
+        url: "https://en.wikipedia.org/wiki/Princess_Mononoke"
+    },
+    {
+        src: mononoke02,
+        url: "https://en.wikipedia.org/wiki/Princess_Mononoke"
+    },
+    {
+        src: mononoke03,
+        url: "https://en.wikipedia.org/wiki/Princess_Mononoke"
+    },
+    {
+        src: nausicaa,
+        url: "https://en.wikipedia.org/wiki/Nausicaä_of_the_Valley_of_the_Wind_(film)"
+    },
+    {
+        src: ponyo01,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo02,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo03,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo04,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo05,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo06,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo07,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: ponyo08,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: poppyHill,
+        url: Studio_Ghibli
+    },
+    {
+        src: runMononoke,
+        url: "https://en.wikipedia.org/wiki/Princess_Mononoke"
+    },
+    {
+        src: smoking,
+        url: Studio_Ghibli
+    },
+    {
+        src: sophieNight,
+        url: "https://en.wikipedia.org/wiki/Howl's_Moving_Castle_(film)"
+    },
+    {
+        src: sophieRun,
+        url: "https://en.wikipedia.org/wiki/Howl's_Moving_Castle_(film)"
+    },
+    {
+        src: sosuke02,
+        url: "https://en.wikipedia.org/wiki/Ponyo"
+    },
+    {
+        src: totoroPls,
+        url: "https://en.wikipedia.org/wiki/My_Neighbor_Totoro"
+    },
+    {
+        src: totoroRain,
+        url: "https://en.wikipedia.org/wiki/My_Neighbor_Totoro"
+    },
+    {
+        src: waterReflection,
+        url: Studio_Ghibli
+    },
+    {
+        src: heart,
+        url: "https://en.wikipedia.org/wiki/Howl's_Moving_Castle_(film)"
+    },
+    {
+        src: rainbow,
+        url: Studio_Ghibli
+    },
+];
 
 export const Main = () => {
     return (
         <Layout>
             <section>
-                <img src={arietty} alt="studio ghibli"/>
-                <img src={butterfly} alt="studio ghibli"/>
-                <img src={cat} alt="studio ghibli"/>
-                <img src={dragon} alt="studio ghibli"/>
-                <img src={ephemeris} alt="studio ghibli"/>
-                <img src={farming} alt="studio ghibli"/>
-                <img src={forestspirit} alt="studio ghibli"/>
-                <img src={lake} alt="studio ghibli"/>
-                <img src={heart} alt="studio ghibli"/>
-                <img src={howl} alt="studio ghibli"/>
-                <img src={howlasleep} alt="studio ghibli"/>
-                <img src={howllook} alt="studio ghibli"/>
-                <img src={kaguya} alt="studio ghibli"/>
-                <img src={kaguya02} alt="studio ghibli"/>
-                <img src={kaguya03} alt="studio ghibli"/>
-                <img src={kaguyafarm} alt="studio ghibli"/>
-
-                <img src={kiki01} alt="studio ghibli"/>
-                <img src={kiki02} alt="studio ghibli"/>
-                <img src={kiki03} alt="studio ghibli"/>
-                <img src={mononoke01} alt="studio ghibli"/>
-                <img src={mononoke02} alt="studio ghibli"/>
-                <img src={mononoke03} alt="studio ghibli"/>
-                <img src={nausicaa} alt="studio ghibli"/>
-                <img src={ponyo01} alt="studio ghibli"/>
-                <img src={ponyo02} alt="studio ghibli"/>
-                <img src={ponyo03} alt="studio ghibli"/>
-                <img src={ponyo04} alt="studio ghibli"/>
-                <img src={ponyo05} alt="studio ghibli"/>
-                <img src={ponyo06} alt="studio ghibli"/>
-                <img src={ponyo07} alt="studio ghibli"/>
-                <img src={ponyo08} alt="studio ghibli"/>
-                <img src={poppyHill} alt="studio ghibli"/>
-                <img src={rainbow} alt="studio ghibli"/>
-                <img src={smoking} alt="studio ghibli"/>
-                <img src={runMononoke} alt="studio ghibli"/>
-                <img src={sophieNight} alt="studio ghibli"/>
-                <img src={sophieRun} alt="studio ghibli"/>
-                <img src={totoroPls} alt="studio ghibli"/>
-                <img src={totoroRain} alt="studio ghibli"/>
-                <img src={waterReflection} alt="studio ghibli"/>
-                <img src={sosuke02} alt="studio ghibli"/>
+                {gifs.map(gif => {
+                    return <img src={gif.src} alt="studio ghibli" onClick={() => {
+                        if (gif.url == "https://en.wikipedia.org/wiki/Studio_Ghibli") {
+                            alert("Here is the main Ghibli wiki, dont know which movie this gif is from");
+                        }
+                        window.open(gif.url);
+                    }} />
+                })}
             </section>
         </Layout>
     )
