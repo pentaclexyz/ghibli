e3acac62642b3b54c6faa3b9e577f3c26adec97c
pentacle <pentacle@pentacle.xyz>
2022-02-10 12:52:35 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
         return <footer>
             <article className={"footer-content flex-group space-between flex-align-center"}>
                 <div>© 2000 - {new Date().getFullYear()} Studio Ghibli - this is a fan site. All images have been sourced from the public domain via google dot com.
-                    <p>Built for Ghibli enjoyers by <a href={"https://twitter.com/0xpentacle"} target={"_blank"} rel="noopener noreferrer">0xPentacle</a></p>
+                    <p>Built for Ghibli enjoyers by <a href={"https://twitter.com/pentaclexyz"} target={"_blank"} rel="noopener noreferrer">pentacle</a></p>
                 </div>
 
             </article>
